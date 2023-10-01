fc8d1551f12445cc0a2e74e7b155429889673273
21DP4KLogi <14DPKLogins@rvt.lv>
2023-10-01 18:47:24 +0300

    Added test pages

---
+++ src/index/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { createApp, ref } from 'vue'
+    const msg = ref('Hello World!')
+    function onButtonClick() {
+        window.location = '/test';
+    }
+</script>
+
+<template>
+    <h1>{{msg}}</h1>
+    <button v-on:click="onButtonClick()">Go to another page</button>
+</template>
+
+<style>
+</style>
---
+++ src/test/Test.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { createApp, ref } from 'vue'
+    const msg = ref('World of hello!')
+    function onButtonClick() {
+        window.location = '/';
+    }
+</script>
+
+<template>
+    <h1>{{msg}}</h1>
+    <button v-on:click="onButtonClick()">Go back to the previous page</button>
+</template>
+
+<style>
+</style>
