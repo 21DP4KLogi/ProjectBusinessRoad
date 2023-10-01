e02c0956bc9dbb353e953996b3cde6d7a2f457f2
21DP4KLogi <14DPKLogins@rvt.lv>
2023-10-01 18:47:51 +0300

    Configured to build vue as separate html files

--- src/ProjectBusinessRoad.nim
+++ src/ProjectBusinessRoad.nim
@@ -1,6 +1,14 @@
 import jester
 import std/segfaults
 
+settings:
+  staticDir = "dist"
+
+proc readHtml(dirName: string): string =
+  return readFile("dist/src/" & dirName & "/index.html")
+
 routes:
   get "/":
-    resp readFile("index.html")
+    resp readHtml("index")
+  get "/test":
+    resp readHtml("test")
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const { resolve } = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index/index.html'),
+        test: resolve(__dirname, 'src/test/index.html')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
