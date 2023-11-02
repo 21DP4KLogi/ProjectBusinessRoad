b3b31a82983ff394ff44dd7ab6fd44951e546a4a
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2023-11-02 09:30:09 +0000

    Added non-functional POST stuff to register page

--- src/register/Register.vue
+++ src/register/Register.vue
@@ -1,6 +1,22 @@
 <script setup>
-    function RegisterInfoSubmitted() {
-        window.location.href = "/test"
+    async function SendRegisterInfo() {
+        const username = document.getElementById("usernameInput");
+        const password = document.getElementById("passwordInput");
+        const confirmPassword = document.getElementById("confirmPassInput");
+        if (password.value != confirmPassword.value) {
+            console.log("Passwords don't match!");
+            return;
+        }
+        await fetch(
+            "/Register",
+            method = "POST",
+            body= JSON.stringify({
+                username: username.value,
+                password: password.value
+                }
+            )
+        )
+
     }
 </script>
 
@@ -8,14 +24,14 @@
     <h1 id="title"><a href="/" title="Click to go back to the Main Page">Business Road</a></h1>
     <h3>Register to Business Road</h3>
     <p class="inputboxtitle">Username</p>
-    <input type="text">
+    <input id="usernameInput" type="text">
     <button>Check availability</button>
     <p class="inputboxtitle">Password</p>
-    <input type="password">
+    <input id="passwordInput" type="password">
     <p class="inputboxtitle">Confirm password</p>
-    <input type="password">
+    <input id="confirmPassInput" type="password">
     <br>
-    <button v-on:click="RegisterInfoSubmitted()" id="infosubmitbutton">Register</button>
+    <button v-on:click="SendRegisterInfo()" id="infosubmitbutton">Register</button>
     <p><i>Already have an account? <a href="/login">Login here!</a></i></p>
 </template>
 
