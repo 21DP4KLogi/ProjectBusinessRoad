eb70cfb864c9762206f39b79894fd164679e6993
21DP4KLogi <14DPKLogins@rvt.lv>
2023-11-27 19:53:04 +0200

    Adjusted spacing

--- src/index/subtabs/Description.vue
+++ src/index/subtabs/Description.vue
@@ -1,6 +1,6 @@
 <template>
     <h3>This is a description for the game</h3>
-    <p>Call me Ishmael. Some years ago—never mind how long precisely—having
+    <p>&emsp;Call me Ishmael. Some years ago—never mind how long precisely—having
 little or no money in my purse, and nothing particular to interest me
 on shore, I thought I would sail about a little and see the watery part
 of the world. It is a way I have of driving off the spleen and
