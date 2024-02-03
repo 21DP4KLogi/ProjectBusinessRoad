6761576501f438f962a8f6f4ccc18b2a2ff0b0d8
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-02-03 12:56:09 +0000

    Added semicolons to JS to follow standards

--- public/script.js
+++ public/script.js
@@ -1,58 +1,57 @@
-var global = {}
-fetchSubtabs()
+var global = {};
+fetchSubtabs();
 
 // This approach might cause problems if the User's internet is too slow,
 // perhaps a loading screen could be useful.
 async function fetchSubtabs() {
-    global.subtabRegister = await fetchComponent("/component/subtabRegister.html")
-    global.subtabLogin = await fetchComponent("/component/subtabLogin.html")
-    global.subtabDesc = await fetchComponent("/component/subtabDesc.html")
+    global.subtabRegister = await fetchComponent("/component/subtabRegister.html");
+    global.subtabLogin = await fetchComponent("/component/subtabLogin.html");
+    global.subtabDesc = await fetchComponent("/component/subtabDesc.html");
 }
 
-
 async function fetchComponent(route) {
-    let response = await fetch(route)
+    let response = await fetch(route);
     if (response.ok) {
-        return await response.text()
+        return await response.text();
     } else {
-        return str("Error " + str(response.status))
+        return str("Error " + str(response.status));
     }
 }
 
 function setOpenedSubtab(subtab) {
-    const bottomLeft = document.getElementById("bottomleft")
+    const bottomLeft = document.getElementById("bottomleft");
     switch (subtab) {
         case "login":
-            bottomLeft.innerHTML = global.subtabLogin
-            break
+            bottomLeft.innerHTML = global.subtabLogin;
+            break;
         case "register":
-            bottomLeft.innerHTML = global.subtabRegister
-            break
+            bottomLeft.innerHTML = global.subtabRegister;
+            break;
         case "desc":
-            bottomLeft.innerHTML = global.subtabDesc
-            break
+            bottomLeft.innerHTML = global.subtabDesc;
+            break;
     }
 }
 
 async function getMOTD() {
     const response = await fetch("/motd");
     const textresp = await response.text();
-    document.getElementById("motdQuote").innerHTML = textresp
+    document.getElementById("motdQuote").innerHTML = textresp;
 }
 
 function setStatusMessage(message, color) {
-    const statusText = document.getElementById("requestStatusText")
-    statusText.innerHTML = message
-    switch(color) {
+    const statusText = document.getElementById("requestStatusText");
+    statusText.innerHTML = message;
+    switch (color) {
         case "default":
-            statusText.className = "defaultColor"
-            break
+            statusText.className = "defaultColor";
+            break;
         case "warning":
-            statusText.className = "warningColor"
-            break
+            statusText.className = "warningColor";
+            break;
         case "success":
-            statusText.className = "successColor"
-            break
+            statusText.className = "successColor";
+            break;
     }
 }
 
@@ -60,20 +59,20 @@ async function SendLoginInfo() {
     const usernameField = document.getElementById("usernameInput");
     const passwordField = document.getElementById("passwordInput");
     let response = await fetch("/login/submitinfo", {
-        method : "POST",
-        body : JSON.stringify({
+        method: "POST",
+        body: JSON.stringify({
             username: usernameField.value,
             password: passwordField.value
-            })
-        }
-    )
+        })
+    }
+    );
     if (response.ok) {
-        setStatusMessage("Success! Going to the game page...", "success")
-        window.location.href = "/game"
+        setStatusMessage("Success! Going to the game page...", "success");
+        window.location.href = "/game";
     } else if (response.status == 400) {
-        setStatusMessage("Incorrect login information", "warning")
+        setStatusMessage("Incorrect login information", "warning");
     } else {
-        setStatusMessage(str("HTTP error, status code: " + str(response.status)), "warning")
+        setStatusMessage(str("HTTP error, status code: " + str(response.status)), "warning");
     }
 
 }
@@ -83,46 +82,46 @@ async function SendRegisterInfo() {
     const passwordField = document.getElementById("passwordInput");
     const confirmPasswordField = document.getElementById("confirmPassInput");
     if (passwordField.value.length < 8) {
-        setStatusMessage("Your password needs to be atleast 8 characters long.", "warning")
-        return
+        setStatusMessage("Your password needs to be atleast 8 characters long.", "warning");
+        return;
     }
     if (passwordField.value != confirmPasswordField.value) {
-        setStatusMessage("Passwords don't match.", "Warning")
-        return
+        setStatusMessage("Passwords don't match.", "Warning");
+        return;
     }
     let response = await fetch("/register/submitinfo", {
-        method : "POST",
-        body : JSON.stringify({
+        method: "POST",
+        body: JSON.stringify({
             username: usernameField.value,
             password: passwordField.value
-            })
-        }
-    )
+        })
+    }
+    );
     if (response.ok) {
-        setStatusMessage("Successfully registered! Please log in.", "success")
+        setStatusMessage("Successfully registered! Please log in.", "success");
     } else if (response.status == 400) {
-        setStatusMessage("Register info not valid.", "warning")
+        setStatusMessage("Register info not valid.", "warning");
     } else {
-        setStatusMessage(str("HTTP error, status code: " + str(response.status)), "warning")
+        setStatusMessage(str("HTTP error, status code: " + str(response.status)), "warning");
     }
 }
 
 async function CheckNameAvailability() {
     const usernameField = document.getElementById("usernameInput");
     if (usernameField.value == "") {
-        setStatusMessage("Input a name to check it's availability", "default")
-        return
+        setStatusMessage("Input a name to check it's availability", "default");
+        return;
     }
     let response = await fetch("/register/checkname", {
-        method : "POST",
-        body : usernameField.value
-        }
-    )
+        method: "POST",
+        body: usernameField.value
+    }
+    );
     if (response.ok) {
-        setStatusMessage("Name is available!", "success")
+        setStatusMessage("Name is available!", "success");
     } else if (response.status == 400) {
-        setStatusMessage("Name is not available.", "warning")
+        setStatusMessage("Name is not available.", "warning");
     } else {
-        setStatusMessage(str("HTTP error, status code: " + str(response.status)), "warning")
+        setStatusMessage(str("HTTP error, status code: " + str(response.status)), "warning");
     }
 }
