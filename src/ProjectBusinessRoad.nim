01a3c889336dec1eeaef22296a7cbdcca85d7852
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-03-20 11:40:08 +0000

    Fixed server response for JQuery script

--- src/ProjectBusinessRoad.nim
+++ src/ProjectBusinessRoad.nim
@@ -119,7 +119,9 @@ routes:
     resp $playerQuery.money
   
   get "/jquery.js":
-    setHeader(responseHeaders, "Content-Encoding", "g-zip")
-    resp compress(readfile("public/jquery-3.7.1.min.js"), BestSpeed, dfGzip)
+    setHeader(responseHeaders, "Content-Encoding", "gzip")
+    resp compress(readfile("public/jquery-3.7.1.min.js"), BestSpeed, dfGzip), contentType = "application/javascript"
+    # Content-Type added as it would otherwise return as text/html and browsers wont run it
+    # Although text/javascript is more recommended, jester uses application/javascript, so i do aswell for consistency
 
 runForever()
