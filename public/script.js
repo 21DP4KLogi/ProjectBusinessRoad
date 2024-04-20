060064365b4828dfec66ba22da1b0e54224a7f16
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-04-20 16:15:40 +0300

    Redesigned frontend to use the backend changes

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
-var global = {};
+// var global = {};
 
 // This approach might cause problems if the User's internet is too slow,
 // perhaps a loading screen could be useful.
 
 async function fetchInitialData() {
-  global.mainPage = document.body.innerHTML; // Main page is saved in global upon loading so that a request is not required
+  // global.mainPage = document.body.innerHTML; // Main page is saved in global upon loading so that a request is not required
   await getMOTD();
-  global.subtabRegister = $.get("/component/subtabRegister.html");
-  global.subtabLogin = $.get("/component/subtabLogin.html");
-  global.subtabDesc = $.get("/component/subtabDesc.html");
+  // global.subtabRegister = $.get("/component/subtabRegister.html");
+  // global.subtabLogin = $.get("/component/subtabLogin.html");
+  // global.subtabDesc = $.get("/component/subtabDesc.html");
 }
 
 async function getMainPage() {
@@ -30,12 +30,15 @@ async function fetchComponent(route) {
 function setOpenedSubtab(subtab) {
   const bottomLeft = $("#bottomleft");
   switch (subtab) {
-    case "login":
-      bottomLeft.load("/component/subtabLogin.html");
+    case "play":
+      bottomLeft.load("/component/subtabPlay.html");
       break;
-    case "register":
-      bottomLeft.load("/component/subtabRegister.html");
-      break;
+    // case "login":
+    //   bottomLeft.load("/component/subtabLogin.html");
+    //   break;
+    // case "register":
+    //   bottomLeft.load("/component/subtabRegister.html");
+    //   break;
     case "desc":
       bottomLeft.load("/component/subtabDesc.html");
       break;
@@ -61,128 +64,123 @@ function setStatusMessage(message, color) {
   }
 }
 
-async function SendLoginInfo() {
-  const username = $("#usernameInput").val();
-  const password = $("#passwordInput").val();
-  const saveCookieCheckboxChecked = $("#SaveLoginCookieBox").is(":checked");
-  if (username == "" || password == "") {
-    setStatusMessage("Please input both the Username and Password", "warning");
+async function LogIn() {
+  // const username = $("#usernameInput").val();
+  // const password = $("#passwordInput").val();
+  // const saveCookieCheckboxChecked = $("#SaveLoginCookieBox").is(":checked");
+  // if (username == "" || password == "") {
+  //   setStatusMessage("Please input both the Username and Password", "warning");
+  //   return;
+  // }
+  // $.post(
+  //   "/auth/login/submitinfo",
+  //   JSON.stringify({
+  //     username: username,
+  //     password: password,
+  //   }),
+  //   function (response, status) {
+  //     if (status == "success") {
+  //       switch (response) {
+  //         case "Failure":
+  //           setStatusMessage("Incorrect login information", "warning");
+  //           break;
+  //         case "NameNotFound":
+  //           setStatusMessage("Username not found", "warning");
+  //           break;
+  //         default:
+  //           if (saveCookieCheckboxChecked) {
+  //             Cookies.set("authToken", response, {
+  //               expires: 7,
+  //               sameSite: "strict",
+  //               secure: true,
+  //             });
+  //           }
+  //           global.authToken = response;
+  //           setStatusMessage("Success! Going to the game page...", "success");
+  //           openGamePage();
+  //           break;
+  //       }
+  //     } else {
+  //       setStatusMessage(
+  //         String("HTTP error, status code: " + String(status)),
+  //         "warning",
+  //       );
+  //     }
+  //   },
+  // );
+  const inputtedCode = $("#loggingIn > input").val();
+  const rememberMeBoxChecked = $("#SaveLoginCookieBox").is(":checked");
+  if (inputtedCode.length != 8) {
+    setStatusMessage("An account code is 8 characters long.", "warning");
     return;
   }
-  $.post(
-    "/auth/login/submitinfo",
-    JSON.stringify({
-      username: username,
-      password: password,
-    }),
-    function (response, status) {
-      if (status == "success") {
-        switch (response) {
-          case "Failure":
-            setStatusMessage("Incorrect login information", "warning");
-            break;
-          case "NameNotFound":
-            setStatusMessage("Username not found", "warning");
-            break;
-          default:
-            if (saveCookieCheckboxChecked) {
-              Cookies.set("authToken", response, {
-                expires: 7,
-                sameSite: "strict",
-                secure: true,
-              });
-            }
-            global.authToken = response;
-            setStatusMessage("Success! Going to the game page...", "success");
-            openGamePage();
-            break;
-        }
-      } else {
-        setStatusMessage(
-          String("HTTP error, status code: " + String(status)),
-          "warning",
-        );
-      }
-    },
-  );
+  $.get("/auth/login", {code: inputtedCode, remember: rememberMeBoxChecked})
+    .done(() => {
+      openGamePage();
+    })
+    .fail(() => {
+      setStatusMessage("Failure to log in, idk why", "warning");
+    });
 }
 
-async function SendRegisterInfo() {
-  const username = $("#usernameInput").val();
-  const password = $("#passwordInput").val();
-  const confirmPassword = $("#confirmPassInput").val();
-  if (password.length < 8) {
-    setStatusMessage(
-      "Your password needs to be atleast 8 characters long.",
-      "warning",
-    );
-    return;
-  }
-  if (password != confirmPassword) {
-    setStatusMessage("Passwords don't match.", "Warning");
-    return;
-  }
-  $.post(
-    "/auth/register/submitinfo",
-    JSON.stringify({
-      username: username,
-      password: password,
-    }),
-    (response, status) => {
-      if (status == "success") {
-        switch (response) {
-          case "Success":
-            setStatusMessage(
-              "Successfully registered! Please log in.",
-              "success",
-            );
-            break;
-          case "NameAlreadyTaken":
-            setStatusMessage("Username is already taken.", "warning");
-            break;
-        }
-      } else {
-        setStatusMessage("Error, status: " + status, "warning");
-      }
-    },
-  );
+async function RegisterAccount() {
+  $("#registration > button")
+    .text("Requesting new account...")
+    .attr("disabled", "true");
+  $.get("/auth/register", (data) => {
+    $("#registration > h1").text(data);
+  })
+    .done(() => {
+      $("#registration > button").text("Account created!");
+    })
+    .fail(() => {
+      setStatusMessage("An unknown error has occured.");
+      $("#registration > button").text("Try again").attr("disabled", "");
+    });
 }
 
-async function CheckNameAvailability() {
-  const username = $("#usernameInput").val();
-  if (username == "") {
-    setStatusMessage("Input a name to check it's availability", "default");
-    return;
-  }
-  $.post("/auth/register/checkname", username, (response, status) => {
-    if (status == "success") {
-      switch (response) {
-        case "NameIsAvailable":
-          setStatusMessage("Name is available!", "success");
-          break;
-        case "NameIsTaken":
-          setStatusMessage("Name is not available.", "warning");
-          break;
-      }
-    } else {
-      setStatusMessage("Error, status: " + status, "warning");
-    }
-  });
-}
+// async function CheckNameAvailability() {
+//   const username = $("#usernameInput").val();
+//   if (username == "") {
+//     setStatusMessage("Input a name to check it's availability", "default");
+//     return;
+//   }
+//   $.post("/auth/register/checkname", username, (response, status) => {
+//     if (status == "success") {
+//       switch (response) {
+//         case "NameIsAvailable":
+//           setStatusMessage("Name is available!", "success");
+//           break;
+//         case "NameIsTaken":
+//           setStatusMessage("Name is not available.", "warning");
+//           break;
+//       }
+//     } else {
+//       setStatusMessage("Error, status: " + status, "warning");
+//     }
+//   });
+// }
 
 async function openGamePage() {
   $(document.body).load("/component/gamePage.html");
 }
 
 async function logOut() {
-  $.post("/auth/logout", global.authToken, () => {
-    getMainPage(),
-      Cookies.remove("authToken", {
-        expires: 7,
-        sameSite: "strict",
-        secure: true,
-      });
-  });
+  // $.post("/auth/logout", global.authToken, () => {
+  //   getMainPage(),
+  //     Cookies.remove("authToken", {
+  //       expires: 7,
+  //       sameSite: "strict",
+  //       secure: true,
+  //     });
+  // });
+  $.get("/auth/logout")
+    .done(() => getMainPage())
+    .fail(() => {
+      alert(
+        "Failed to request a logout, you may have to delete the cookie from the browser manually.",
+      );
+    });
 }
 
 async function getMoney() {
