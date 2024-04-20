476e92d3dd81bedbb24b669f4ea1476f240df915
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-04-20 16:57:50 +0300

    Removed unneeded code

--- public/script.js
+++ public/script.js
@@ -1,15 +1,6 @@
-// var global = {};
-
-// This approach might cause problems if the User's internet is too slow,
-// perhaps a loading screen could be useful.
-
-async function fetchInitialData() {
-  // global.mainPage = document.body.innerHTML; // Main page is saved in global upon loading so that a request is not required
-  await getMOTD();
-  // global.subtabRegister = $.get("/component/subtabRegister.html");
-  // global.subtabLogin = $.get("/component/subtabLogin.html");
-  // global.subtabDesc = $.get("/component/subtabDesc.html");
-}
+$(document).ready(() => {
+  getMOTD();
+});
 
 async function getMainPage() {
   await $(document.body).load("/component/mainPage.html", () => {
@@ -33,12 +24,6 @@ function setOpenedSubtab(subtab) {
     case "play":
       bottomLeft.load("/component/subtabPlay.html");
       break;
-    // case "login":
-    //   bottomLeft.load("/component/subtabLogin.html");
-    //   break;
-    // case "register":
-    //   bottomLeft.load("/component/subtabRegister.html");
-    //   break;
     case "desc":
       bottomLeft.load("/component/subtabDesc.html");
       break;
@@ -65,49 +50,6 @@ function setStatusMessage(message, color) {
 }
 
 async function LogIn() {
-  // const username = $("#usernameInput").val();
-  // const password = $("#passwordInput").val();
-  // const saveCookieCheckboxChecked = $("#SaveLoginCookieBox").is(":checked");
-  // if (username == "" || password == "") {
-  //   setStatusMessage("Please input both the Username and Password", "warning");
-  //   return;
-  // }
-  // $.post(
-  //   "/auth/login/submitinfo",
-  //   JSON.stringify({
-  //     username: username,
-  //     password: password,
-  //   }),
-  //   function (response, status) {
-  //     if (status == "success") {
-  //       switch (response) {
-  //         case "Failure":
-  //           setStatusMessage("Incorrect login information", "warning");
-  //           break;
-  //         case "NameNotFound":
-  //           setStatusMessage("Username not found", "warning");
-  //           break;
-  //         default:
-  //           if (saveCookieCheckboxChecked) {
-  //             Cookies.set("authToken", response, {
-  //               expires: 7,
-  //               sameSite: "strict",
-  //               secure: true,
-  //             });
-  //           }
-  //           global.authToken = response;
-  //           setStatusMessage("Success! Going to the game page...", "success");
-  //           openGamePage();
-  //           break;
-  //       }
-  //     } else {
-  //       setStatusMessage(
-  //         String("HTTP error, status code: " + String(status)),
-  //         "warning",
-  //       );
-  //     }
-  //   },
-  // );
   const inputtedCode = $("#loggingIn > input").val();
   const rememberMeBoxChecked = $("#SaveLoginCookieBox").is(":checked");
   if (inputtedCode.length != 8) {
@@ -139,41 +81,11 @@ async function RegisterAccount() {
     });
 }
 
-// async function CheckNameAvailability() {
-//   const username = $("#usernameInput").val();
-//   if (username == "") {
-//     setStatusMessage("Input a name to check it's availability", "default");
-//     return;
-//   }
-//   $.post("/auth/register/checkname", username, (response, status) => {
-//     if (status == "success") {
-//       switch (response) {
-//         case "NameIsAvailable":
-//           setStatusMessage("Name is available!", "success");
-//           break;
-//         case "NameIsTaken":
-//           setStatusMessage("Name is not available.", "warning");
-//           break;
-//       }
-//     } else {
-//       setStatusMessage("Error, status: " + status, "warning");
-//     }
-//   });
-// }
-
 async function openGamePage() {
   $(document.body).load("/component/gamePage.html");
 }
 
 async function logOut() {
-  // $.post("/auth/logout", global.authToken, () => {
-  //   getMainPage(),
-  //     Cookies.remove("authToken", {
-  //       expires: 7,
-  //       sameSite: "strict",
-  //       secure: true,
-  //     });
-  // });
   $.get("/auth/logout")
     .done(() => getMainPage())
     .fail(() => {
